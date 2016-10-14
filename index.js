aa20f0aa51c08638291a8e46d63eb0127efc41df
Matthew Wolff <mwolff3@lsu.edu>
2016-10-14 00:15:05 -0500

    python runs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const multer = require('multer')
 const childProcess = require('child_process')
+const fs = require('fs')
 
 var app = express()
 var uploads = multer({ dest: 'uploads/' })
@@ -9,8 +10,12 @@ var uploads = multer({ dest: 'uploads/' })
 app.use(express.static('static'))
 
 app.post('/uploads', uploads.any(), (req, res, next) => {
-  console.log(`Starting job for audio file ${req.files[0].filename}`)
-  const py = childProcess.spawn('python', ['plot_vocal_separation.py', req.files[0].path])
+  // Prepare file structure
+  const dirname = 'uploads/'+req.files[0].originalname+'/'
+  fs.mkdirSync(dirname)
+  const path = dirname+req.files[0].originalname
+  fs.renameSync(req.files[0].path, path)
+  const py = childProcess.spawn('python', ['vocal_separation_test.py', dirname, path])
   py.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`)
   })
