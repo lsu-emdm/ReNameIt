0a50d9da122fe0f9ecdd0201a36b6bdf96db1d53
Matthew Wolff <mwolff3@lsu.edu>
2016-12-01 20:27:45 -0600

    proper angular and nexus interaction

--- static/jobController.js
+++ static/jobController.js
@@ -4,13 +4,23 @@ angular.module('renotate').controller("JobController", ["$scope", "Job", ($scope
   $scope.loaded = false
   // promises...
   $scope.jobdata.$promise.then((result) => {
+    // once the job data has been received, make tone players out of them
     $scope.orig = new Tone.Player(window.location.origin+"/"+result.file, () => {
-      nx.add("waveform")
-      waveform1.setBuffer($scope.orig.buffer._buffer)
+      // then once tone is done, retransform the nexus widgets because of how angular works
+
+      nx.transform(document.getElementById("originalWaveform"))
+      //nexus has defined this now
+      originalWaveform.setBuffer($scope.orig.buffer._buffer)
     }).toMaster()
     window.orig = $scope.orig
-    $scope.foreground = new Tone.Player(window.location.origin+"/"+"foregrounds/"+result.name+"_"+result.multer_id).toMaster()
-    $scope.background = new Tone.Player(window.location.origin+"/"+"backgrounds/"+result.name+"_"+result.multer_id).toMaster()
+    $scope.foreground = new Tone.Player(window.location.origin+"/"+"foregrounds/"+result.name+"_"+result.multer_id, () => {
+      nx.transform(document.getElementById("foregroundWaveform"))
+      foregroundWaveform.setBuffer($scope.foreground.buffer._buffer)
+    }).toMaster()
+    $scope.background = new Tone.Player(window.location.origin+"/"+"backgrounds/"+result.name+"_"+result.multer_id, () => {
+      nx.transform(document.getElementById("backgroundWaveform"))
+      backgroundWaveform.setBuffer($scope.background.buffer._buffer)
+    }).toMaster()
   })
   $scope.play = () => {
     $scope.orig.start()
